ff2e781fae307e7a54485cf3757a966e3a616875
cipher <sarah77nashat77@gmail.com>
2023-01-17 02:54:26 +0200

    features and more flexibility

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ var close=document.getElementById('close');
 close.addEventListener('click', function () {
     nav.style.display = 'none'
 })
+
+
+///////singie pro////// 
+var min=document.getElementById('minIMG');
+var smallimg=document.getElementsByClassName('sallIMG');
+
+smallimg[0].onclick=function(){
+min.src=smallimg[0].src;
+};
+smallimg[1].onclick=function(){
+ min.src=smallimg[1].src;
+ console.log("2222")
+};
+smallimg[2].onclick=function(){
+min.src=smallimg[2].src;
+};
+smallimg[3].onclick=function(){
+ min.src=smallimg[3].src;
+};        
